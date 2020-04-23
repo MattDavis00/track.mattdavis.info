5c5190edbd278efd6883972bbd6b5712c3f2605b
Matt Davis <mw.davis@hotmail.co.uk>
2020-04-23 16:57:46 +0100

    Added a working and styled map using the google maps javascript api and the google-maps-react package.

---
+++ frontend/src/MapContainer.js
@@ -0,0 +1,240 @@
+import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+const mapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#212121"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#212121"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.country",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#1b1b1b"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#2c2c2c"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#8a8a8a"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#373737"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#3c3c3c"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway.controlled_access",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#4e4e4e"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#3d3d3d"
+        }
+      ]
+    }
+];
+
+class MapContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          stores: [{latitude: 47.49855629475769, longitude: -122.14184416996333},
+                  {latitude: 47.359423, longitude: -122.021071},
+                  {latitude: 47.2052192687988, longitude: -121.988426208496},
+                  {latitude: 47.6307081, longitude: -122.1434325},
+                  {latitude: 47.3084488, longitude: -122.2140121},
+                  {latitude: 47.5524695, longitude: -122.0425407}]
+        }
+    }
+
+    displayMarkers = () => {
+        return this.state.stores.map((store, index) => {
+          return <Marker key={index} id={index} position={{
+           lat: store.latitude,
+           lng: store.longitude
+         }}
+         onClick={() => console.log("You clicked me!")} />
+        })
+    }
+
+    _mapLoaded(mapProps, map) {
+        map.setOptions({
+           styles: mapStyle
+        })
+    }
+
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={8}
+                initialCenter={{ lat: 47.444, lng: -122.176}}
+                onReady={(mapProps, map) => this._mapLoaded(mapProps, map)}
+            >
+                {this.displayMarkers()}
+            </Map>
+        );
+
+    }
+
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBK0SNa9cPIfx600KxgNviICCCBGDdHW10'
+})(MapContainer);
