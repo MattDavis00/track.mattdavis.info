2906538335589260499d5398f32846199192fffc
Matt Davis <mw.davis@hotmail.co.uk>
2020-05-03 20:18:36 +0100

    Added the ability to submit tracking nodes. The session values for userID and loggedIn are now set when the /use-token path is successfully used.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,38 @@
 //////////////////////////////////////////////////////////////////////////////////////
 "use strict";
 
+// Express
 const express = require('express');
+const app = express();
+const port = 4020;
 
 // Setup DB connection
 var mysql = require('mysql');
 var con = require('./connect');
 var cred = require('./credentials');
+
 const https = require('https');
-const app = express();
-const port = 4020;
+
+// Client Session
+var session = require('express-session')
+var MySQLStore = require('express-mysql-session')(session);
+var sessionStore = new MySQLStore({}, con);
+// Set Session Parameters
+app.use(session({
+    key: "track.mattdavis.info cookie",
+    secret: cred.sessionSecret,
+    saveUninitialized: false,
+    store: sessionStore,
+    resave: false,
+    cookie: {
+        maxAge: 3600000, // 1 Hour
+        secure: cred.secureCookie,
+        httpOnly: true
+    }
+}));
+
+// Parse body of request as a JSON object.
+app.use(express.json());
 
 // Heath Check
 app.listen(port, () => console.log(`Track API is listening on port ${port}!`));
@@ -22,7 +45,10 @@ app.get('/', function(req, res){
 try {
 
     // https://auth.mattdavis.info/api/auth?redirectURL=https%3A%2F%2Ftrack.mattdavis.info%2F&tokenURL=https%3A%2F%2Ftrack.mattdavis.info%2Fapi%2Fuse-token
+    // https://auth.mattdavis.info/api/check-token?token=
 
+
+    //////////////////////// Use Token ////////////////////////
     app.get('/use-token', function (req, res){
         let token = req.query.token;
         let path = "/api/check-token?token=" + token;
@@ -47,6 +73,8 @@ try {
                 console.log("LoggedIn: " + data.loggedIn);
                 if(data.loggedIn === true) {
                     res.send("User was authenticated with this token!: " + jsonData);
+                    req.session.loggedIn = true;
+                    req.session.userID = data.userID;
                 } else {
                     res.send("User could not be authenticated using SSO.");
                 }
@@ -60,8 +88,45 @@ try {
         
         authReq.end();
 
-        // https://auth.mattdavis.info/api/check-token?token=
     });
+
+    //////////////////////// Submit Node ////////////////////////
+    app.post('/submit-node', function(req, res) {
+        const data = req.body;
+
+        let idIsCorrect = false;
+        let sql = "SELECT * FROM device WHERE device_id = ?";
+        con.query(sql, [data.deviceID], function (err, result) {
+            if (!err)
+            {
+                console.log("Number of devices returned: " + result.length);
+                if (result.length === 1) {
+                    insertNode(data, req, res);
+                } else {
+                    res.send("Device ID is incorrect.");
+                }
+            } else {
+                res.send("Device ID is incorrect.");
+            }
+        });
+    });
+
+    function insertNode(data, req, res) {
+        let sql = "INSERT INTO node (device_id, longitude, latitude) VALUES (?,?,?)";
+        con.query(sql, [data.deviceID, data.longitude, data.latitude], function (err, result) {
+            if (!err)
+            {
+                console.log("Number of nodes inserted: " + result.affectedRows);
+                if (result.affectedRows === 1) {
+                    res.send(JSON.stringify({"success": true, "nodeID": result.insertId, "errors": []}));
+                } else {
+                    res.send("Node could not be inserted.");
+                }
+            } else {
+                res.send("Node could not be inserted.");
+            }
+        });
+    }
 
 } catch(err) {
     console.log(err);
