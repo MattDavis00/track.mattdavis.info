91cb8a1aa21d7433065120b696ea067baf3b1314
Matt Davis <mw.davis@hotmail.co.uk>
2020-05-04 22:08:56 +0100

    Added a side menu which will be used to add new devices and to logout

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import './App.css';
+import SideMenu from './SideMenu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretSquareRight, faCaretSquareLeft } from '@fortawesome/free-solid-svg-icons'
 
 class NavBar extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.state = {menuVisible: false};
+    }
+
+    toggleMenu() {
+        this.setState({menuVisible: !this.state.menuVisible});
+    }
+
     render() {
+
+        let menuIcon;
+        let menu;
+
+        if (this.state.menuVisible) {
+            menuIcon = <FontAwesomeIcon onClick={this.toggleMenu} icon={faCaretSquareRight} />;
+            menu = <SideMenu />;
+        } else {
+            menuIcon = <FontAwesomeIcon onClick={this.toggleMenu} icon={faCaretSquareLeft} />;
+            menu = null;
+        }
+
         return (
-            <div className="navbar">
-                <h3>track.mattdavis.info</h3>
-                <h3 href="">Logout</h3>
-                {/* <a href="">Logout</a> */}
+            <div>
+                <div className="navbar">
+                    <h3>track.mattdavis.info</h3>
+                    <h3 className="white-button">{menuIcon}</h3>
+                </div>
+                {menu}
             </div>
         );
 
---
+++ frontend/src/SideMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SideMenu extends React.Component
+{
+    render() {
+        return (
+            <div className="sideMenu">
+                <div className="flex-column">
+                    <div>The Side Menu</div>
+                    <h3>The Side Menu</h3>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SideMenu;
